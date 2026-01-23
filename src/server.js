7e4d1c6e62d26dfa3326aa1b744e2a1157c40dcf
SergeyKorobka <s.korobka@goit.ua>
2026-01-23 23:00:14 +0200

    validation

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
+import { errors } from 'celebrate';
+
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
@@ -16,6 +18,7 @@ app.use(logger);
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
+app.use(errors());
 app.use(errorHandler);
 
 await connectMongoDB();
---
+++ src/validations/notesValidation.js
@@ -0,0 +1,38 @@
+import { Segments, Joi } from 'celebrate';
+import { TAGS } from '../constants/tags.js';
+import { isValidObjectId } from 'mongoose';
+
+const validatorObjectId = (value, helper) =>
+  isValidObjectId(value) ? value : helper.message('Invalid id format');
+
+export const noteIdSchema = {
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(validatorObjectId).required(),
+  }),
+};
+
+export const getAllNotesSchema = {
+  [Segments.QUERY]: Joi.object({
+    page: Joi.number().integer().min(1).default(1),
+    perPage: Joi.number().integer().min(5).max(20).default(10),
+    tag: Joi.string().valid(...TAGS),
+    search: Joi.string().allow(''),
+  }),
+};
+
+export const createNoteSchema = {
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1).required(),
+    content: Joi.string().allow(''),
+    tag: Joi.string().valid(...TAGS),
+  }),
+};
+
+export const updateNoteSchema = {
+  ...noteIdSchema,
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1),
+    content: Joi.string().allow(''),
+    tag: Joi.string().valid(...TAGS),
+  }).min(1),
+};
